42abba25b442f28e0eeda5646a5c1bb871738039
safa <safamahmoudmohamed@gmail.com>
2024-08-02 00:08:46 +0300

    update

--- src/app/layout/exercises/exercises.component.ts
+++ src/app/layout/exercises/exercises.component.ts
@@ -1,12 +1,85 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { DragDropModule, CdkDragRelease } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-exercises',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, DragDropModule],
   templateUrl: './exercises.component.html',
-  styleUrl: './exercises.component.css'
+  styleUrls: ['./exercises.component.css']
 })
 export class ExercisesComponent {
+  showIndividual: boolean = false;
+  showSeveral: boolean = false;
+  isAddingChoice = false;
+  newChoiceText = '';
+  currentQuestionType: 'single' | 'multiple' | null = null;
+  showSuccessMessage = false;
+  showErrorMessage = false; 
+  
+  singleChoiceOptions: { text: string, isEditable: boolean }[] = [];
+  multipleChoiceOptions: { text: string, isEditable: boolean }[] = [];
+  questionText = ''; 
 
+  onDragReleased(event: CdkDragRelease, buttonType: string) {
+    if (buttonType === 'singleChoice') {
+      this.showIndividual = true;
+      this.showSeveral = false;
+      this.currentQuestionType = 'single';
+    } else if (buttonType === 'multiChoice') {
+      this.showIndividual = false;
+      this.showSeveral = true;
+      this.currentQuestionType = 'multiple';
+    }
+    event.source._dragRef.reset();
+  }
+
+  showAddChoiceForm(type: 'single' | 'multiple') {
+    this.isAddingChoice = true;
+    this.currentQuestionType = type;
+  }
+
+  saveChoice() {
+    if (this.newChoiceText.trim()) {
+      if (this.currentQuestionType === 'single') {
+        this.singleChoiceOptions.push({ text: this.newChoiceText, isEditable: false });
+      } else if (this.currentQuestionType === 'multiple') {
+        this.multipleChoiceOptions.push({ text: this.newChoiceText, isEditable: false });
+      }
+      this.newChoiceText = '';
+      this.isAddingChoice = false; 
+    }
+  }
+
+  cancelAddChoice() {
+    this.newChoiceText = '';
+    this.isAddingChoice = false;
+  }
+
+  saveQuestion() {
+    if (!this.questionText.trim()) {
+      this.showErrorMessage = true; 
+      setTimeout(() => this.showErrorMessage = false, 3000); 
+      return;
+    }
+    console.log('Saved question with choices:', this.currentQuestionType === 'single' ? this.singleChoiceOptions : this.multipleChoiceOptions);
+    this.showSuccessMessage = true; 
+    setTimeout(() => this.showSuccessMessage = false, 3000); 
+  }
+
+  cancelQuestion() {
+    if (this.currentQuestionType === 'single') {
+      this.singleChoiceOptions = [];
+    } else if (this.currentQuestionType === 'multiple') {
+      this.multipleChoiceOptions = [];
+    }
+    this.newChoiceText = '';
+    this.isAddingChoice = false;
+    this.showIndividual = false;
+    this.showSeveral = false;
+    this.currentQuestionType = null; 
+    this.questionText = ''; 
+  }
 }
--- src/app/layout/subject/subject.component.ts
+++ src/app/layout/subject/subject.component.ts
@@ -1,31 +1,3 @@
-/*import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
-import { Subject } from '../../models/subject';
-import { SubjectService } from '../../requestApi/subject.service';
-import { CommonModule } from '@angular/common';
-
-@Component({
-  selector: 'app-subject',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './subject.component.html',
-  styleUrl: './subject.component.css'
-})
-export class SubjectComponent implements OnInit {
-  subjects: Subject[] = [];
-  newSubject : Subject = {} as Subject;
-  constructor(private subjectService : SubjectService)
-  {}
-  ngOnInit(): void {
-    this.getSubjects()
-  }
-  getSubjects(){
-    this.subjectService.getSubjects().subscribe(sub => {
-      this.subjects = sub;
-    })
-  }
-}*/
-
 
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
